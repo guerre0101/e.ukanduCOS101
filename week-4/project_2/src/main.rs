394fab8321e60bb52af43b11cab0a5e1cb9cf7c6
guerre0101 <ethanukandu@gmail.com>
2026-09-17 15:26:40 +0100

    (test)

--- week-4/project_2/src/main.rs
+++ week-4/project_2/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let mut input1 = String::new();
 let mut input2 = String::new();
 
+// input your age and state whether youre experienced or not
 println!("Enter your age");
 io::stdin().read_line(&mut input1).expect("Not a valid string");
 let age:f32 = input1.trim().parse().expect("Not a valid number");
@@ -11,6 +12,7 @@ println!("Are you experienced?");
 io::stdin().read_line(&mut input2).expect("Not a valid string");
 let experience:bool = input2.trim().to_lowercase() == "yes";
 
+//runs a comparison to allocate the salary
 
 if age >= 40.0 && experience{
     println!("Your salary is N1,560,000");
